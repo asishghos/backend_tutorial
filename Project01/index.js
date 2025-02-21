2fb54d98b828a3e38bdc1bd0feafb9050572098d
asishghos <ccasish01@gmail.com>
2025-02-21 22:42:58 +0530

    new

--- Project01/index.js
+++ Project01/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const path = require("path");
-const { fileURLToPath } = require("url");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,8 +9,21 @@ app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  fs.readdir("./files", (err, files) => {
+  fs.readdir(`./files`, (err, files) => {
     res.render("index", { files: files });
+  });
+});
+
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  fs.writeFile(`./files/${req.body.title}.txt`, req.body.details, (err) => {
+    res.redirect("/");
+  });
+});
+
+app.get(`files/:filename`, (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, (err, filedata) => {
+    console.log(filedata.toString());
   });
 });
 
